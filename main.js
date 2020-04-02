3beda3a166327057133dd63d7c6b81955148eeda
Seth Newby <seth.newby27@gmail.com>
2020-04-02 14:42:50 -0500

    added fourth button w/ javascript link to website

--- main.js
+++ main.js
@@ -4,3 +4,7 @@ button.onclick = function() {
   let name = prompt('What is your name?');
   alert('Hello ' + name + ', nice to see you!');
 }
+
+function visitPage(){
+        window.location='https://barackobama.com/';
+    }
